0e3a38caa04e57c64fd4fd5c23839a59d9d5528b
Arjun Pundir <148452492+ArjunPundir231105@users.noreply.github.com>
2026-09-22 07:56:03 +0000

    Time: 1702 ms (34.13%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,20 @@
+SELECT c.category,
+       COUNT(a.account_id) AS accounts_count
+FROM (
+    SELECT 'Low Salary' AS category
+    UNION ALL
+    SELECT 'Average Salary'
+    UNION ALL
+    SELECT 'High Salary'
+) c
+LEFT JOIN (
+    SELECT account_id,
+           CASE
+               WHEN income < 20000 THEN 'Low Salary'
+               WHEN income <= 50000 THEN 'Average Salary'
+               ELSE 'High Salary'
+           END AS category
+    FROM Accounts
+) a
+ON c.category = a.category
+GROUP BY c.category;
