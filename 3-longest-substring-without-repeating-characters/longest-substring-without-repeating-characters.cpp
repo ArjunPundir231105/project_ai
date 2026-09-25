e69474165cc0a6a4e87b01b5849315599d009547
Arjun Pundir <148452492+ArjunPundir231105@users.noreply.github.com>
2026-09-25 12:31:52 +0000

    Time: 45 ms (60.85%) | Memory: 19.1 MB (69.52%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,8 +5,7 @@ public:
         int l = 0;
         int r = 0;
         int n = s.length();
-        if(n==0) return 0;
-        int ans=-1;
+        int ans=0;
         while(r<n){
             if(mp.find(s[r])!=mp.end())  l=max(l,mp[s[r]]+1);  
             mp[s[r]]=r;
