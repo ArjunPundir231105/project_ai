41a151a09706ca6699d28a519081b828ad1cc26c
Arjun Pundir <148452492+ArjunPundir231105@users.noreply.github.com>
2026-09-26 12:42:05 +0000

    Time: 4 ms (45.88%) | Memory: 32.7 MB (62.20%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int sol(vector<int>& nums, int goal) {
-        if (goal < 0) return 0;
-
+    int sol(vector<int>& nums, int goal){
+        if(goal<0) return 0;
         int l = 0;
-        int sum = 0;
-        long long count = 0;
-
-        for (int r = 0; r < nums.size(); r++) {
-            sum += nums[r];
-
-            while (sum > goal) {
-                sum -= nums[l];
+        int r = 0;
+        int n = nums.size();
+        long long sum=0;
+        long long count=0;
+        while(r<n){
+            sum+=nums[r];
+            while(sum>goal){
+                sum=sum-nums[l];
                 l++;
             }
-
-            count += (r - l + 1);
+            count+=(r-l+1);
+            r++;
         }
-
-        return count;
+      return count;  
     }
-
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return sol(nums, goal) - sol(nums, goal - 1);
+        return sol(nums,goal)-sol(nums,goal-1);
     }
 };
